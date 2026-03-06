715a941d21d511361500ec005f75b3057f718307
JayXie9499 <50140372+JayXie9499@users.noreply.github.com>
2026-03-06 11:15:07 +0800

    feat(eslint): enforce code style with ESLint rules
    - Add `padding-line-between-statements` rule to enforce code style.

--- eslint.config.ts
+++ eslint.config.ts
@@ -12,7 +12,25 @@ export default defineConfigWithVueTs(
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
   ...pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-
   ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
-  skipFormatting
+  skipFormatting,
+  {
+    rules: {
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: 'import', next: '*' },
+        { blankLine: 'never', prev: 'import', next: 'import' },
+        { blankLine: 'always', prev: 'export', next: '*' },
+        { blankLine: 'always', prev: '*', next: 'export' },
+        { blankLine: 'always', prev: ['var', 'let', 'const'], next: '*' },
+        { blankLine: 'always', prev: '*', next: ['var', 'let', 'const'] },
+        { blankLine: 'never', prev: ['var', 'let', 'const'], next: ['var', 'let', 'const'] },
+        { blankLine: 'always', prev: 'block-like', next: '*' },
+        { blankLine: 'always', prev: '*', next: 'block-like' },
+        { blankLine: 'never', prev: 'if', next: 'if' },
+        { blankLine: 'any', prev: 'for', next: 'for' },
+        { blankLine: 'always', prev: '*', next: 'return' }
+      ]
+    }
+  }
 );
